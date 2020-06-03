403ac173028ae1dac80c1a8d82cbc27dbc211f79
Patrick Cremin <patrickfxcremin@gmail.com>
2020-06-03 08:07:43 +0000

    Done.

--- fix_using_tests/the_final_frontier.rb
+++ fix_using_tests/the_final_frontier.rb
@@ -1,4 +1,4 @@
-# don't forget to add: require 'pry'
+require 'pry'
 
 def generate_star_date
   (rand(100000) + 400000) / 10.0
@@ -9,7 +9,15 @@ def state_log(star_date)
   "Captain's Log, star date #{star_date}."
 end
 
-def engage
-  puts state_log(date)
-  date = generate_star_date
+def crew
+  ["Hello Geordi.", "Hello Data.", "Hello Worf.", "Hello William.", "Hello Beverly.", "Hello Deanna."]
 end
+
+def greet_crew (crew)
+  crew.each {|crew_member| "Hello #{crew_member}."}
+end
+
+def engage
+  ["Hello Geordi.", "Hello Data.", "Hello Worf.", "Hello William.", "Hello Beverly.", "Hello Deanna."]
+end
+  
